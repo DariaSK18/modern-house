16a3618a416e102e1c71688cb5d89c901da5640c
Daria Steblovska <darias1896.96@gmail.com>
2025-10-07 02:29:58 +0100

    layout loader function implemented (using fetch)

--- js/layout-loader.js
+++ js/layout-loader.js
@@ -1,1 +1,14 @@
 // funcs for header and footer (using fetch)
+
+document.addEventListener("DOMContentLoaded", () => {
+  const insertHTML = (selector, filePath) => {
+    const container = document.getElementById(selector);
+    if (container) {
+      fetch(filePath)
+      .then((res) => res.text())
+      .then(data => container.innerHTML = data)
+    }
+  };
+  insertHTML("header-placeholder", "./src/header.html");
+  insertHTML("footer-placeholder", "./src/footer.html");
+});
