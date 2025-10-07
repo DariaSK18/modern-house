bb8bb6ad22d2f703aaf950f53c7866ee241b3c8c
Daria Steblovska <darias1896.96@gmail.com>
2025-10-07 07:35:43 +0100

    swiper added to hero section (commented)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
 // main js funcs
+
+// Initialize Swiper
+
+// var swiper = new Swiper(".mySwiper", {
+//   direction: "vertical",
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   mousewheel: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
